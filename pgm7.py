ea5d839994df548216ef6b5170c6ae87c0488a99
Anurag Mishra <167283320+AnuragMishra1234@users.noreply.github.com>
2026-06-22 06:02:55 +0530

    Update pgm7.py

--- pgm7.py
+++ pgm7.py
@@ -1,3 +1,5 @@
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
 tokenizer = T5Tokenizer.from_pretrained("t5-small")
 model = T5ForConditionalGeneration.from_pretrained("t5-small")
 
